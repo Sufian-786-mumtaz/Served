a3f986ff36bcc540d837d0c2d2841447858084db
Sufian-786-mumtaz <sufianmumtaz786@gmail.com>
2021-09-20 13:22:23 +0500

    served

---
+++ Assests/js/Custom.js
@@ -0,0 +1,33 @@
+window.onscroll=function(){
+    myfunction()
+};
+var navbar = document.getElementById("mynavbar");
+function myfunction(){
+    if(window.pageYOffset>=100){
+        navbar.classList.add("sticky")
+    }
+    else{
+        navbar.classList.remove("sticky");
+    }
+}
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:15,
+    nav:true,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+
