c8664ddf8ca5745e3f7ee280d3903b29c7e3cab5
Sufian-786-mumtaz <sufianmumtaz786@gmail.com>
2021-09-22 12:42:10 +0500

    safyan

--- Assests/js/Custom.js
+++ Assests/js/Custom.js
@@ -30,4 +30,9 @@ $('.owl-carousel').owlCarousel({
     }
 })
 
-
+$(document).ready(function() {
+    $('#headerVideoLink').magnificPopup({
+      type:'inline',
+      midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+    });         
+  });
